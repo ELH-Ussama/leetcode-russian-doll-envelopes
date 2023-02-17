318de79ba358a96876b8b875844081302436f6d7
ELH_Ussama <50701642+NW-Ussama@users.noreply.github.com>
2023-02-17 12:44:18 +0100

    feat: stupid algorithm based on size

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from typing import List
 
 
 class Solution:
+    @staticmethod
+    def envelope_size(envelope: List[int]) -> int:
+        return envelope[0] + envelope[1]
+
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        return 0
+        sizes = []
+
+        for envelope in envelopes:
+            sizes.append(self.envelope_size(envelope))
+
+        return len(set(sizes))
 
 
 if __name__ == '__main__':
