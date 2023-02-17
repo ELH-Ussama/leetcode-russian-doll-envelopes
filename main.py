c7194b139a29387324566c643df695f7604c8ef3
ELH_Ussama <50701642+NW-Ussama@users.noreply.github.com>
2023-02-17 12:44:18 +0100

    refactor: use class attribute envelopes in class Solution

--- main.py
+++ main.py
@@ -48,41 +48,41 @@ class Envelope:
 
 
 class Solution:
-    @staticmethod
-    def init_envelopes(envelopes: List[List[int]]) -> List[Envelope]:
+    envelopes: List[Envelope] = None
+
+    def init_envelopes(self, envelopes: List[List[int]]) -> None:
         all_envelopes: List[Envelope] = []
 
         for i in range(len(envelopes)):
             envelope = envelopes[i]
             all_envelopes.append(Envelope(i, envelope[0], envelope[1]))
 
-        return all_envelopes
+        self.envelopes = all_envelopes
 
-    @staticmethod
-    def get_envelope_can_contain_map(all_envelopes: List[Envelope]) -> Dict[int, List[int]]:
+    def get_envelope_can_contain_map(self) -> Dict[int, List[int]]:
         envelope_can_contain_map: Dict[int: List[int]] = {}
 
-        for i in range(len(all_envelopes)):
-            envelope = all_envelopes[i]
-            can_be_contained = envelope.filter_can_contain(all_envelopes)
+        for i in range(len(self.envelopes)):
+            envelope = self.envelopes[i]
+            can_be_contained = envelope.filter_can_contain(self.envelopes)
             ids = [e.id for e in can_be_contained]
             envelope_can_contain_map[i] = ids
 
         return envelope_can_contain_map
 
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        all_envelopes = self.init_envelopes(envelopes)
+        self.init_envelopes(envelopes)
 
-        envelope_can_contain_map = self.get_envelope_can_contain_map(all_envelopes)
+        envelope_can_contain_map = self.get_envelope_can_contain_map()
 
         for envelope_id in envelope_can_contain_map:
             can_contain = envelope_can_contain_map[envelope_id]
-            can_contain_nodes = [all_envelopes[envelope_id] for envelope_id in can_contain]
-            all_envelopes[envelope_id].children = can_contain_nodes
+            can_contain_nodes = [self.envelopes[envelope_id] for envelope_id in can_contain]
+            self.envelopes[envelope_id].children = can_contain_nodes
 
-        root_envelopes = [e.id for e in all_envelopes if not e.can_fit_in_at_least_one(all_envelopes)]
+        root_envelopes = [e.id for e in self.envelopes if not e.can_fit_in_at_least_one(self.envelopes)]
 
-        nodes_depths = [all_envelopes[i].depth() for i in root_envelopes]
+        nodes_depths = [self.envelopes[i].depth() for i in root_envelopes]
 
         return max(nodes_depths)
 
